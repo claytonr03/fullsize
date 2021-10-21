f0c40c6bdb36877ea82fdca7356a8943092793b6
Clayton Rayment <clayton-git@zero-three.tech>
2021-10-20 22:12:22 -0600

    add circles grid discovery code
    Notes:
    Added code to discover an asymmetric circles grid
    Added code to display the point overlay of the circles grid
    
    Signed-off-by: Clayton Rayment <clayton-git@zero-three.tech>

--- calibratedcamera.py
+++ calibratedcamera.py
@@ -5,8 +5,11 @@
 
 import cv2
 import picamera
+# import picamera.array
 import io
 import json
+
+ASYMMETRIC_CIRCLES_SHAPE = (4, 11)
 
 class CalibratedPiCamera:
   scale = (1, 1)
@@ -33,7 +36,7 @@ class CalibratedPiCamera:
   # Capture and return uncalibrated image
   def capture_raw(self):
     if self.type == "pi":
-      raw_capture = picamera.PiRGBArray(self.cam)
+      raw_capture = picamera.array.PiRGBArray(self.cam)
       self.cam.capture(raw_capture, format="bgr")
       return raw_capture.array
     
@@ -48,7 +51,7 @@ class CalibratedPiCamera:
   # Capture and return calibrated image
   def capture_calibrated(self):
     raw_image = self.capture_raw()
-    
+
     pass 
 
   # Calibration function to determine camera intrinsics
@@ -72,9 +75,32 @@ class CalibratedPiCamera:
     corrected_image = cv2.undistort(raw_image, camera_matrix, distortion_coefficient, None, newcamera)
     return corrected_image
 
+  def store_raw_capture(self, filename):
+    raw_image = self.capture_raw()
+    cv2.imwrite(filename, raw_image)
+
 
   def display_raw_capture(self):
     raw_image = self.capture_raw()
-    cv2.namedWindow("raw capture")
+    cv2.namedWindow('raw capture')
     cv2.imshow('raw capture', raw_image)
     cv2.waitKey(0)
+
+  def find_blobs(self, image):
+    ret, points = cv2.findCirclesGrid(image, ASYMMETRIC_CIRCLES_SHAPE, flags=cv2.CALIB_CB_ASYMMETRIC_GRID)
+    if ret:
+      return points
+    return None
+
+  def display_blobs(self):
+    raw_image = self.capture_raw()
+    cv2.namedWindow('blobs')
+    blobs = self.find_blobs(raw_image)
+    # print(blobs)
+    if blobs.any():
+      drawn_image = cv2.drawChessboardCorners(raw_image, ASYMMETRIC_CIRCLES_SHAPE, blobs, True)
+      cv2.imshow('blobs', drawn_image)
+    else:
+      cv2.imshow('blobs', raw_image)
+    cv2.waitKey(0)
+
