51f6ca06808ff425e307efb48a9a7b523905bd5a
Clayton Rayment <clayton-git@zero-three.tech>
2021-10-24 11:53:11 -0600

    add contourmaker svg generation routine
    Notes:
    This commit adds the svg contour generation routine to the ContourMaker
    application layer.
    
    TODO: Scale SVGs appropriately
    
    Signed-off-by: Clayton Rayment <clayton-git@zero-three.tech>

--- contourmaker.py
+++ contourmaker.py
@@ -6,6 +6,8 @@ import os
 import email, smtplib, ssl
 import sys
 
+from imagingstand import ImagingStand
+
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
@@ -14,19 +16,131 @@ from email.mime.text import MIMEText
 # Imports PIL module  
 from PIL import Image 
 
+class ContourMaker:
 
-def auto_canny(image, sigma=0.33):
+  imager = None
 
-	# compute the median of the single channel pixel intensities
-	v = np.median(image)
+  def __init__(self):
+    self.imager = ImagingStand(17, 27, "webcam", "./camera_calibration_data_generated.json")
 
-	# apply automatic Canny edge detection using the computed median
-	lower = int(max(0, (1.0 - sigma) * v))
-	upper = int(min(255, (1.0 + sigma) * v))
-	edged = cv2.Canny(image, lower, upper)
 
-	# return the edged image
-	return edged
+  def auto_canny(self, image, sigma=0.33):
+
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
+  
+  def generate_contour_svg(self, filepath, toolnumber):
+    cv2.namedWindow('generate_contour_svg')
+
+    # -------------------------
+    # Capture Top Image
+    # -------------------------
+    frame = self.imager.capture_top()
+    scale_percentx = 69.5
+    scale_percenty = 69.5
+    width = int(frame.shape[1] * scale_percentx / 100)
+    height = int(frame.shape[0] * scale_percenty / 100)
+    dsize = (width, height)
+    frame = cv2.resize(frame, dsize)
+    #Y is first with Yo and Y distance
+    #frameT = frame[400:2600, 1000:3300]
+    # frameT = frame[120:4000, 550:3500]
+
+    cv2.imshow('generate_contour_svg', frame)
+    cv2.waitKey(0)
+    
+    # ------------------------
+    # Capture Bottom Image
+    # ------------------------
+    frame = self.imager.capture_bottom()
+    #scale_percentx = 69.5
+    #scale_percenty = 69.5
+    width = int(frame.shape[1] * scale_percentx / 100)
+    height = int(frame.shape[0] * scale_percenty / 100)
+    dsize = (width, height)
+    #frame = cv2.resize(frame, ds)ize)
+    # frame = frame[120:4000, 550:3500]
+    
+    cv2.imshow('generate_contour_svg', frame)
+    cv2.waitKey(0)
+
+    # ------------------------
+    # Generate Contours
+    # ------------------------
+    fgGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.threshold(fgGray, 0, 255,
+      cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    #cv2.namedWindow("test",cv2.WND_PROP_FULLSCREEN)
+    #cv2.setWindowProperty("test",cv2.WND_PROP_FULLSCREEN,cv2.CV_WINDOW_FULLSCREEN)
+
+    thresh = cv2.erode(thresh, None, iterations=5)#4
+    thresh = cv2.dilate(thresh, None, iterations=1)#4
+    cv2.imshow('generate_contour_svg', thresh)
+    cv2.waitKey(0)
+
+    blurred = cv2.GaussianBlur(thresh, (3, 3), 0)
+    edged = cv2.Canny(thresh, 50, 130)
+    edged = cm1.auto_canny(thresh)
+
+    cv2.imshow('generate_contour_svg', edged)
+    cv2.waitKey(0)
+
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+      cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    
+    # ------------------------
+    # Generate SVG
+    # ------------------------
+    total = 0
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    #print(timestr)
+
+    c = max(cnts, key=cv2.contourArea) #max contour
+    f = open(filepath+'/'+toolnumber+'_'+timestr+'_vectors.svg', 'w+')
+    f.write('<svg width="4000" height="4000" xmlns="http://www.w3.org/2000/svg">')
+    # loop over the contours one by one
+    for c in cnts:
+      if cv2.contourArea(c) < 500:	#or cv2.contourArea(c) > 200:
+        continue
+    #	print(" contour {}".format(cv2.contourArea(c)))
+      x,y,w,h = cv2.boundingRect(c)
+    #	print(x)
+    #	print(y)
+    #	print(w)
+    #	print(h)
+      cv2.drawContours(frame, [c], -1, (204, 0, 255), 2)
+      total += 1
+      f.write('<path d="M')
+      for i in range(len(c)):
+        if i == 0:
+          x1, y1 = c[i][0]
+        #print(c[i][0])
+        x, y = c[i][0]
+        #print(x)
+        f.write(str(x)+  ' ' + str(y)+' ')
+      f.write(str(x1)+  ' ' + str(y1)+' ')
+      f.write('"/>')
+
+    f.write('</svg>')
+    f.close()
+
+
+if __name__ == "__main__":
+  cm1 = ContourMaker()
+  cm1.generate_contour_svg("./", "1")
+
+exit(1)
 
 if len(sys.argv) > 1:
  usb = sys.argv[1]
@@ -52,7 +166,8 @@ x_dim = sys.argv[4]
 y_dim = sys.argv[5]
 
 ###########################
-frame = cv2.imread("/home/pi/Documents/unfish/corrected_images/TopView.jpg",1)
+# frame = cv2.imread("/home/pi/Documents/unfish/corrected_images/TopView.jpg",1)
+frame = cm1.imager.capture_top()
 scale_percentx = 69.5
 scale_percenty = 69.5
 width = int(frame.shape[1] * scale_percentx / 100)
@@ -64,7 +179,8 @@ frame = cv2.resize(frame, dsize)
 frameT = frame[120:4000, 550:3500]
 ###########################
  
-frame = cv2.imread("/home/pi/Documents/unfish/corrected_images/workingImage.jpg",1)
+# frame = cv2.imread("/home/pi/Documents/unfish/corrected_images/workingImage.jpg",1)
+frame = cm1.imager.capture_bottom()
 #scale_percentx = 69.5
 #scale_percenty = 69.5
 width = int(frame.shape[1] * scale_percentx / 100)
@@ -93,7 +209,7 @@ cv2.waitKey(0)
 
 blurred = cv2.GaussianBlur(thresh, (3, 3), 0)
 edged = cv2.Canny(thresh, 50, 130)
-edged = auto_canny(thresh)
+edged = cm1.auto_canny(thresh)
 
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
